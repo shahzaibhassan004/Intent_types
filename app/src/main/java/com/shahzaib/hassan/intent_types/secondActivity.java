107efb00ee53df9c415c0c141635808437b5f423
SHAHZAIB HASSAN <shahzaibhassan004@gmail.com>
2020-11-03 13:50:57 +0500

    Initial commit

---
+++ app/src/main/java/com/shahzaib/hassan/intent_types/secondActivity.java
@@ -0,0 +1,27 @@
+package com.shahzaib.hassan.intent_types;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class secondActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        button = findViewById(R.id.btncall);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+923008709771"));
+                startActivity(intent);
+            }
+        });
+    }
+}
